3a80444d7d940f9b7416675a402369d012ea2873
Elijah Grote <errriigrote@gmail.com>
2018-01-20 13:41:26 -0800

    adding options menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import { GameModeComponent } from './game-mode/game-mode.component';
 import {TestComponent} from './game-mode/test/test.component';
+import { OptionMenuComponent } from './option-menu/option-menu.component';
 
 const appRoutes: Routes = [
     {path: '', component: MainMenuComponent, pathMatch: 'full'},
     {path: 'Play', component: GameModeComponent, pathMatch: 'full'},
+    {path: 'Options', component: OptionMenuComponent, pathMatch: 'full'},
     {path: 'Page-Not-Found', component: PageNotFoundComponent},
     {path: '**', redirectTo: 'Page-Not-Found'}
 ];
@@ -23,7 +25,8 @@ const appRoutes: Routes = [
     MainMenuComponent,
     GameModeComponent,
     PageNotFoundComponent,
-    TestComponent
+    TestComponent,
+    OptionMenuComponent
   ],
   imports: [
     BrowserModule,
