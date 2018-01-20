a492c7d7a1eb39d472d7f0d959521d649de7f809
Kai <kaiperez@yahoo.com>
2018-01-19 22:00:41 -0800

    Added Bootstrap 3.3 to the project. Added a router to the project that leads to the main menu when '/' is requested

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+const appRoutes: Routes = [
+    {path: '', component: MainMenuComponent, pathMatch: 'full'}
+    //{path: '**', redirectTo: 'Page-Not-Found'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
