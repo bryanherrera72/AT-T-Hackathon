9b0577cb46c4d2dbf2f5723811f19896550b23eb
Elijah Grote <errriigrote@gmail.com>
2018-01-20 11:38:48 -0800

    title page working

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MainMenuComponent implements OnInit {
   title = 'Beat Bop';
   menuItems = ['Play', 'Create Song','User Settings'];
-  routes = ['test', 'test', 'test'];
+  routes = ['Play', 'test', 'test'];
   constructor() { }
 
   ngOnInit() {
