6edd6eacd35d6c64d240ddf969c800e9e24b439a
Elijah Grote <errriigrote@gmail.com>
2018-01-20 12:24:51 -0800

    sound on main menu button click

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.css']
+  
 })
 export class MainMenuComponent implements OnInit {
   title = 'Beat Bop';
   menuItems = ['Play', 'Create Song','User Settings'];
   routes = ['Play', 'test', 'test'];
+  sounds:Howl[];
   constructor() { }
 
+  buttonplay()
+  {
+    this.sounds[0].play();
+    console.log("played audio from button");
+  }
+
   ngOnInit() {
+    this.sounds = [new Howl({src:'../assets/audio/Famoush.wav'})];
   }
 }
